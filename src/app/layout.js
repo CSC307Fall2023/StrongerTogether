b00a7137fff96e8e7d19fa168c77dc08f515e9fd
Zhi He <zhihe122@gmail.com>
2023-11-02 01:05:35 -0700

    Backend: added user apis, events apis, as well as comments for some files and util

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import RootLayout from './RootLayout';
 import AuthProvider from './AuthProvider';
 
 const inter = Inter({ subsets: ['latin'] })
-const COMPANY_NAME = "MyCompany";
+const COMPANY_NAME = "StrongerTogether";
 
 export const metadata = {
   title: COMPANY_NAME
@@ -14,7 +14,7 @@ export default function Layout({ children }) {
     <html lang="en">
       <body>
         <AuthProvider>
-          <RootLayout children={children} title={COMPANY_NAME}/>        
+          <RootLayout children={children} title={COMPANY_NAME}/>  
         </AuthProvider>
       </body>
     </html>
